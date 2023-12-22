713bb17e5e2dd7e69a62a960fd5bb303eb08c3e2
naomielianora <naomitanujaya13@gmail.com>
2023-12-22 15:55:12 +0700

    make home and header

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const PORT = 8080;
+const app = express();
+
+app.listen(PORT, () => {
+    console.log(`Server is ready, listening on port ${PORT}`);
+});
+
+app.set('view engine', 'ejs');
+//dipakai untuk routing path file css, assets
+app.use(express.static('public'));
+
+//page yang pertama kali muncul (home)
+app.get('/', (req, res)=>{
+    res.render('home')
+})
